4dc12b6b9e6140454fdd8a47f4e80232652ff066
Melanie-devv <melanie.bulkan@epsi.fr>
2024-03-08 16:43:55 +0100

    responsive for project image modal window

--- pages/bts.jsx
+++ pages/bts.jsx
@@ -18,8 +18,8 @@ const GithubPage = ({ repos, user }) => {
     <div className={styles.body} style={{ opacity, transition: 'opacity 0.5s' }}>
       <h2>Présentation du BTS SIO</h2><br/><br/>
       <p>Le BTS <b>Services Informatique aux Organisations</b> est un diplôme reconnu par l’état de niveau Bac+2. Il remplace depuis septembre 2011 l’ancien BTS informatique de Gestion (BTS IG).</p><br/>
-      <p>Il s’adresse à ceux qui souhaitent se former en deux ans aux métiers d’administrateur réseau ou de développeur pour intégrer directement le marché du travail ou continuer des études dans le domaine de l’informatique.</p><br />
-      <p>Le programme a évolué en fonction de la place et du rôle des TIC (Technologies d’Information Communication) et propose deux spécialités bien distinctes : le BTS SIO SISR et le BTS SIO SLAM.</p><br/><br/>
+      <p>Il s’adresse à ceux qui souhaitent se former en deux ans aux métiers <b>d’administrateur réseau</b> ou de <b>développeur</b> pour intégrer directement le marché du travail ou continuer des études dans le domaine de l’informatique.</p><br />
+      <p>Le programme a évolué en fonction de la place et du rôle des TIC (Technologies d’Information Communication) et propose deux spécialités bien distinctes : le <b>BTS SIO SISR</b> et le <b>BTS SIO SLAM</b>.</p><br/><br/>
 
       <h3 style={{textAlign: "center"}}>Les options du BTS SIO</h3>
       <div className={styles.optionContainer}>
